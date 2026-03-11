a0ef685576514da10a678116f057c64eae840aa8
Paulo Cesar <paulocesarcoder@gmail.com>
2026-03-11 19:42:17 -0300

    Update Autor.java

--- src/main/java/com/estudos/biblioteca/model/Autor.java
+++ src/main/java/com/estudos/biblioteca/model/Autor.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
